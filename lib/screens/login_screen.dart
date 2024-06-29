316567ab8dc3fe3bd2a298b1727ea2759c208487
dadaxon11 <dadaxonmakh@gmail.com>
2024-06-29 19:22:59 +0500

    has a new changes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:goread/services/hive_service.dart';
 import 'package:goread/services/log_service.dart';
 import 'package:goread/utils/colors.dart';
-
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -53,16 +53,107 @@ class _LoginScreenState extends State<LoginScreen> {
                           margin: const EdgeInsets.only(bottom: 20, top: 30),
                           child: TextField(
                             controller: controllers[index],
+                            obscureText: index == 4 ? false : true,
                             decoration: InputDecoration(
                                 hintText: hintTexts[index],
-                                hintStyle: const TextStyle(color: Colors.grey)),
+                                hintStyle: const TextStyle(color: Colors.grey),
+
+                            ),
                           ));
                     },
                   ),
                 ),
-                const SizedBox(height: 18),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: ColorsOwn.mainColor,
+                        fixedSize: const Size(170, 50)),
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(color: ColorsOwn.subColor, fontSize: 20),
+                    ),
+                  ),
+                ),
+                 SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        color: ColorsOwn.blackColor,
+                      ),
+                    ),
+                   const SizedBox(width: 10),
+                   const Text("or sign in with",style: TextStyle(color: ColorsOwn.blackColor,fontSize: 16,fontWeight: FontWeight.bold),),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        color: ColorsOwn.blackColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                Row(
+                  children: [
+                    Expanded(child: Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.black)
+                      ),
+                      child:const Icon(Icons.mail_outline_outlined,size: 40,),
+                    ),
+                    ),
+                   const SizedBox(width: 12),
+                    Expanded(child: Container(
+                      padding: const EdgeInsets.all(10),
 
-               ElevatedButton(onPressed: (){}, child: const Text("Sign Up",style: TextStyle(color: ColorsOwn.subColor,fontSize: 20),))
+                      height: 60,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.black)
+                      ),
+                      child: Image.asset('assets/icons/google_logo.png'),
+                    ),
+                    ),
+                    const SizedBox(width: 12),
+                    Expanded(child:Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.black)
+                      ),
+                      child: const Icon(Icons.facebook,size: 40,),
+
+                    ),),
+                    const SizedBox(width: 12),
+                    Expanded(child: Container(
+                      padding: const EdgeInsets.all(10),
+                      height: 60,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.black)
+                      ),
+                      child: Image.asset("assets/icons/twitter_logo.png"),
+
+                    ),),
+                    const SizedBox(width: 12),
+                    Expanded(child: Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.black)
+                      ),
+                      child: const Icon(Icons.apple,size: 40,),
+
+
+                    ),),
+                  ],
+                )
               ],
             ),
           ]),
